1cf018b068a12f488f41804ad1c0f86604ecda6f
prasadpalled <prasad062003@gmail.com>
2026-07-21 23:37:59 +0530

    Time: 0 ms (100.00%) | Memory: 15.9 MB (9.19%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int pivot=-1,n=nums.size();
+        int pivot = -1, n = nums.size();
         // Find pivot;
-        for(int i=n-2;i>=0;i--){
-           if(nums[i]<nums[i+1]){
-            pivot=i;
-            break;
-           }
-        }
-        cout<<pivot<<endl;
-        if(pivot==-1){
-            sort(nums.begin(),nums.end());
-        }
-        else{
-            for(int i=n-1;i>pivot;i--){
-                if(nums[i]>nums[pivot]){
-                    swap(nums[i],nums[pivot]);
-                    break;
-                }
+        for (int i = n - 2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
+                pivot = i;
+                break;
             }
-            sort(nums.begin()+pivot+1,nums.end());
         }
+        cout << pivot << endl;
+        if (pivot == -1) {
+            sort(nums.begin(), nums.end());
+            return;
+        }
+        for (int i = n - 1; i > pivot; i--) {
+            if (nums[i] > nums[pivot]) {
+                swap(nums[i], nums[pivot]);
+                break;
+            }
+        }
+        sort(nums.begin() + pivot + 1, nums.end());
     }
 };
