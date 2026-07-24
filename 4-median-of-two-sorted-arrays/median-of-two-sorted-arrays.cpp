82e3ba3f3911cdb9ec66a10430cc4a837777f131
prasadpalled <prasad062003@gmail.com>
2026-07-24 15:52:17 +0530

    Time: 0 ms (100.00%) | Memory: 95.2 MB (42.80%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,34 +1,36 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> mergeArray;
-        int i=0,j=0,mid;
-        double result;
-        while(i<nums1.size()&&j<nums2.size()){
-          if(nums1[i]<nums2[j]){
-           mergeArray.push_back(nums1[i]);
-           i++;
-          }
-          else{
-            mergeArray.push_back(nums2[j]);
-            j++;
-          }
+        if(nums1.size()>nums2.size()){
+            return findMedianSortedArrays(nums2,nums1);
         }
-        while(i<nums1.size()){
-           mergeArray.push_back(nums1[i]);
-           i++;
+        int m=nums1.size();
+        int n=nums2.size();
+        
+        int l=0;
+        int h=m;
+        while(l<=h){
+            int p1=l+((h-l)/2);
+            int p2=((m+n+1)/2) - p1;
+            int l1= (p1==0)? INT_MIN : nums1[p1-1];
+            int l2= (p2==0)? INT_MIN : nums2[p2-1];
+            int r1= (p1==m)? INT_MAX : nums1[p1];
+            int r2= (p2==n)? INT_MAX : nums2[p2];
+            if(l1<=r2 && l2<=r1){
+                if((m+n)%2==1){
+                    return max(l1,l2);
+                }
+                else{
+                    return (max(l1,l2)+min(r1,r2))/2.0;
+                }
+            }
+            if(l1>r2){
+                h=p1-1;
+            }
+            else{
+                l=p1+1;
+            }
         }
-        while(j<nums2.size()){
-           mergeArray.push_back(nums2[j]);
-           j++;
-        }
-        mid=(mergeArray.size()-1)/2;
-        if(mergeArray.size()%2==0){
-          result=(mergeArray[mid]+mergeArray[mid+1])/2.0;
-        }
-        else{
-          result=mergeArray[mid];
-        }
-        return result;
+        return -1;
     }
 };
