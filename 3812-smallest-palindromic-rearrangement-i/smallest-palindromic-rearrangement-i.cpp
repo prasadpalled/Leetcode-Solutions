9c0cecdb3e6ec5f430ed23bf0a4c401fb6e9f69f
prasadpalled <prasad062003@gmail.com>
2026-07-28 23:10:23 +0530

    Time: 77 ms (41.32%) | Memory: 71.7 MB (53.85%) - LeetSync

--- 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.cpp
+++ 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.cpp
@@ -1,25 +1,51 @@
 class Solution {
 public:
     string smallestPalindrome(string s) {
-        int len=s.length();
-        int mid=(len-1)/2;
-        string s2;
-        // cout<<mid<<endl;
-        // if(len==1){
-        //     return s;
+        // int len=s.length();
+        // int mid=(len-1)/2;
+        // string s2;
+        // // cout<<mid<<endl;
+        // // if(len==1){
+        // //     return s;
+        // // }
+        // if(len%2==0){
+        //   sort(s.begin(),s.begin()+mid+1);
+        //   s2.append(s.begin(),s.begin()+mid+1);
+        //   reverse(s.begin(),s.begin()+mid+1);
+        //   s2.append(s.begin(),s.begin()+mid+1);
         // }
-        if(len%2==0){
-          sort(s.begin(),s.begin()+mid+1);
-          s2.append(s.begin(),s.begin()+mid+1);
-          reverse(s.begin(),s.begin()+mid+1);
-          s2.append(s.begin(),s.begin()+mid+1);
+        // else{
+        //     sort(s.begin(),s.begin()+mid);
+        //     s2.append(s.begin(),s.begin()+mid);
+        //     s2+=s[mid];
+        //     reverse(s.begin(),s.begin()+mid);
+        //     s2.append(s.begin(),s.begin()+mid);
+        // }
+        // return s2;
+        map<char,int> count;
+        int mid=(s.length()-1)/2;
+        string s2;
+        if(s.length()%2==0){
+         for(int i=0;i<=mid;i++){
+          count[s[i]]++;
+         }
+         for(auto it: count){
+          s2.append(it.second,it.first);
+         }
+         s=s2;
+         reverse(s.begin(),s.end());
+         s2.append(s);
         }
         else{
-            sort(s.begin(),s.begin()+mid);
-            s2.append(s.begin(),s.begin()+mid);
-            s2+=s[mid];
-            reverse(s.begin(),s.begin()+mid);
-            s2.append(s.begin(),s.begin()+mid);
+         for(int i=0;i<mid;i++){
+          count[s[i]]++;
+         }
+         for(auto it: count){
+          s2.append(it.second,it.first);
+         }
+         s=s2+s[mid];
+         reverse(s.begin(),s.end());
+         s2.append(s);
         }
         return s2;
     }
