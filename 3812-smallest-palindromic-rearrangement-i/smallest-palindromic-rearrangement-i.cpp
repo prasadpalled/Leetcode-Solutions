61f67d8a54bd0197a8d028750b222d7134308543
prasadpalled <prasad062003@gmail.com>
2026-07-28 22:52:05 +0530

    Time: 76 ms (41.76%) | Memory: 66.8 MB (72.97%) - LeetSync

--- 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.cpp
+++ 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.cpp
@@ -3,18 +3,24 @@ public:
     string smallestPalindrome(string s) {
         int len=s.length();
         int mid=(len-1)/2;
+        string s2;
         // cout<<mid<<endl;
         // if(len==1){
         //     return s;
         // }
         if(len%2==0){
           sort(s.begin(),s.begin()+mid+1);
-          sort(s.begin()+mid+1,s.end(),greater<char>());
+          s2.append(s.begin(),s.begin()+mid+1);
+          reverse(s.begin(),s.begin()+mid+1);
+          s2.append(s.begin(),s.begin()+mid+1);
         }
         else{
             sort(s.begin(),s.begin()+mid);
-            sort(s.begin()+mid+1,s.end(),greater<char>());
+            s2.append(s.begin(),s.begin()+mid);
+            s2+=s[mid];
+            reverse(s.begin(),s.begin()+mid);
+            s2.append(s.begin(),s.begin()+mid);
         }
-        return s;
+        return s2;
     }
 };
