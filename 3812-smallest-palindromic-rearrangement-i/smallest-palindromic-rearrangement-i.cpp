0f16b19c7345af613f854d041a615b07067d3197
prasadpalled <prasad062003@gmail.com>
2026-08-03 21:05:04 +0530

    Time: 29 ms (64.04%) | Memory: 74.2 MB (36.23%) - LeetSync

--- 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.cpp
+++ 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.cpp
@@ -1,52 +1,27 @@
 class Solution {
 public:
     string smallestPalindrome(string s) {
-        // int len=s.length();
-        // int mid=(len-1)/2;
-        // string s2;
-        // // cout<<mid<<endl;
-        // // if(len==1){
-        // //     return s;
-        // // }
-        // if(len%2==0){
-        //   sort(s.begin(),s.begin()+mid+1);
-        //   s2.append(s.begin(),s.begin()+mid+1);
-        //   reverse(s.begin(),s.begin()+mid+1);
-        //   s2.append(s.begin(),s.begin()+mid+1);
-        // }
-        // else{
-        //     sort(s.begin(),s.begin()+mid);
-        //     s2.append(s.begin(),s.begin()+mid);
-        //     s2+=s[mid];
-        //     reverse(s.begin(),s.begin()+mid);
-        //     s2.append(s.begin(),s.begin()+mid);
-        // }
-        // return s2;
-        map<char,int> count;
-        int mid=(s.length()-1)/2;
-        string s2;
-        if(s.length()%2==0){
-         for(int i=0;i<=mid;i++){
-          count[s[i]]++;
-         }
-         for(auto it: count){
-          s2.append(it.second,it.first);
-         }
-         s=s2;
-         reverse(s.begin(),s.end());
-         s2.append(s);
+        if(s.length()==1){
+            return s;
         }
-        else{
-         for(int i=0;i<mid;i++){
-          count[s[i]]++;
-         }
-         for(auto it: count){
-          s2.append(it.second,it.first);
-         }
-         s=s2+s[mid];
-         reverse(s.begin(),s.end());
-         s2.append(s);
+        vector<int> freq(26,0);
+        string result;
+        int mid=-1;
+        for(int i=0;i<s.length();i++){
+            freq[s[i]-'a']++;
         }
-        return s2;
+        for(int i=0;i<freq.size();i++){
+            result.append(freq[i]/2,i+'a');
+            if(freq[i]%2==1){
+                mid=i;
+            }
+        }
+        string first=result;
+        reverse(result.begin(),result.end());
+        if(mid!=-1){
+          first.push_back(mid+'a');
+        }
+        result=first+result;
+        return result;
     }
 };
