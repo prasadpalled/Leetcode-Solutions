27343cddc5ef6a73b243fd3ade85b296a5214e73
prasadpalled <prasad062003@gmail.com>
2026-08-04 12:23:31 +0530

    Time: 0 ms (100.00%) | Memory: 32.3 MB (75.54%) - LeetSync

--- 4107-find-missing-elements/find-missing-elements.cpp
+++ 4107-find-missing-elements/find-missing-elements.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     vector<int> findMissingElements(vector<int>& nums) {
-        int max= *max_element(nums.begin(),nums.end());
-        int min= *min_element(nums.begin(),nums.end());
-        vector<int> freq(max-min+1,0);
+        sort(nums.begin(),nums.end());
         vector<int> result;
-        for(int i=0;i<nums.size();i++){
-            freq[nums[i]-min]++;
-        }
-        for(int i=0;i<freq.size();i++){
-            if(freq[i]==0){
-                result.push_back(i+min);
-            }
+        int cur=nums[0],i=0;
+        while(i<nums.size()){
+         if(cur==nums[i]){
+            i++;
+            cur++;
+         }
+         else{
+            result.push_back(cur);
+            cur++;
+         }
         }
         return result;
     }
